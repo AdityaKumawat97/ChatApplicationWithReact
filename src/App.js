41cfdacf6741a0a3101b7e7c2fb10b642c91da45
Aditya_kumawat <31047132+AdityaKumawat97@users.noreply.github.com>
2021-06-07 20:37:17 +0530

    adde styles and initilized hat engine

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { ChatEngine } from "react-chat-engine";
+import './app.css'
+
 function App() {
   return (
-    <div className="App">
-      h1
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID="3cd92106-26e2-4afa-ae4d-b85dcb04687b"
+      userName="Aditya"
+      userSecret="123123"
+    />
   );
 }
 
